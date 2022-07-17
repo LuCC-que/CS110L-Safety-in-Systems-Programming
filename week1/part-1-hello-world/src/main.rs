68f02dbbeb9bf81604b04222d602f4841545f3bd
LuCC-que <chen1968493689@gmail.com>
2022-07-17 18:21:53 +0000

    first commit

---
+++ week1/part-1-hello-world/src/main.rs
@@ -0,0 +1,26 @@
+fn main() {
+    println!("Hello, world!");
+}
+
+// fn change_it_up(s: &mut String) {
+//     *s = "goodbye".to_string();
+// }
+   
+// fn make_it_plural(word: &mut String) {
+//     word.push('s');
+// }
+   
+// fn let_me_see(s: &String) {
+//     println!("{}", s);
+// }
+   
+// fn main() {
+//     let mut s = "hello".to_string();
+//     change_it_up(&mut s); //also emphansis this is mutable reference
+//     let_me_see(&s);//emphansis this is reference
+//     make_it_plural(&mut s);
+//     let_me_see(&s);
+//     // let's make it even more plural
+//     s.push('s'); // does this seem strange?
+//     let_me_see(&s);
+// }
